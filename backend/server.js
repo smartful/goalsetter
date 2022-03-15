21069bd594128f10aad4e6da67846b7ac85a5970
smartful <remi_rodrigues@hotmail.com>
2022-03-15 01:19:21 +0100

    Router initialisation

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("Hello les nazes !!!");
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.listen(port, () => console.log(`Server started on PORT : ${port}`));
