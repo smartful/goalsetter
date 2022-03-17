3d8284d43cd9c06d4c67ec2c9c3d770ad5232061
smartful <remi_rodrigues@hotmail.com>
2022-03-17 01:23:59 +0100

    Error management

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -9,6 +9,10 @@ const getGoals = (req, res) => {
 // @route     POST api/goals
 // @access    Private
 const setGoal = (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field !");
+  }
   res.status(201).json({ message: "Create goal" });
 };
 
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/goals", require("./routes/goalRoutes"));
 
+app.use(errorHandler);
 app.listen(port, () => console.log(`Server started on PORT : ${port}`));
