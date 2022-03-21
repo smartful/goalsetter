8fb218ae0766447f6520ab8d3c67e4f01c2dcaaa
smartful <remi_rodrigues@hotmail.com>
2022-03-21 03:52:12 +0100

    connection to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const colors = require("colors");
+
+const { connectDB } = require("./config/db");
 const { errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
