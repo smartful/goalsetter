ab95e8a7e7532765520c61c2447d00e38e016146
smartful <remi_rodrigues@hotmail.com>
2022-03-24 02:07:17 +0100

    Action BDD

---
+++ backend/controllers/userController.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+// @desc      Create a new user
+// @route     POST api/users
+// @access    Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields !");
+  }
+  res.json({ message: "Register User" });
+});
+
+// @desc      Authenticate a user
+// @route     POST api/users/login
+// @access    Public
+const loginUser = asyncHandler(async (req, res) => {
+  res.json({ message: "Login User" });
+});
+
+// @desc      Get user data
+// @route     GET api/users/me
+// @access    Private
+const getMe = asyncHandler(async (req, res) => {
+  res.json({ message: "User data display" });
+});
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getMe,
+};
