f8a0356ec70d05d656fcc51d38c868f7e8e31cd2
smartful <remi_rodrigues@hotmail.com>
2022-03-29 03:00:52 +0200

    user goals protection for delete action

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -67,6 +67,17 @@ const deleteGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found !");
   }
 
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
   await goal.remove();
 
   res.status(200).json({ id: req.params.id });
