9b0ba62b4c54d307d0f7d3eb37a190c20b5b4ca0
smartful <remi_rodrigues@hotmail.com>
2022-04-04 04:31:35 +0200

    Header navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Header from "./components/Header";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1>GoalSetter</h1>
-    </div>
+    <Fragment>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </Fragment>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <Link to="/">GoalSetter</Link>
+      <ul>
+        <li>
+          <Link to="/login">
+            <FaSignInAlt /> Login
+          </Link>
+        </li>
+        <li>
+          <Link to="/register">
+            <FaUser /> Register
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
